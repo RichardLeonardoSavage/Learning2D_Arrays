27a248cb8bad0911dbd3a7223ca4dbee05c2cf55
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 14:56:44 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Learning2D_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // [,] defines that the array is 2D AKA has arrays within the array - remember that [] defines a normal array 
+            int[,] numberGrid = {
+                {1, 2},
+                {3, 4 },
+                {5, 6 },
+                {6, 7 },
+                {7, 8 },
+                {8, 9 },
+                {9, 10 },
+                {10, 11 }
+            };
+
+            Console.WriteLine(numberGrid[0, 0]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
